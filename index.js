db625434fa1e8c8347bcf16be7e1b6d9862eb412
shipovalovaksenia <kseniya.shipovalova@onetwotrip.com>
2025-06-18 21:38:17 +0300

    fix token

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const TelegramApi = require ('node-telegram-bot-api');
 const {gameOptions, againOptions} = require('./options')
 //не просто импорты, т к в package.json "type": "commonjs", а не module. такой формат используется по умолчанию
-// import { gameOptions, againOptions } from './options.js'; - но вроде на такое не ругается
 
-const token = '7765850239:AAHxS1B1BLS8tGQeTCmMqqfx70l8bSBIbW0';
-
-const bot = new TelegramApi (token, {polling: true});
-
+const TG_TOKEN = require('./.env')
+const bot = new TelegramApi (TG_TOKEN, {polling: true});
 const chats = [];
 
 const startGame = async (chatId) => {
     await bot.sendMessage(chatId, 'cейчас я загадаю цифру от 1 до 9, а ты попробуй ее отгадать');
     const random = Math.floor(Math.random() * 10);
-    chats[chatId] = random;
+    chats[chatId] = random;//мутирует это шо
     await bot.sendMessage(chatId, 'отгадывай', gameOptions)
 }
 
@@ -22,8 +19,8 @@ bot.setMyCommands([
     {command: '/game', description: 'давай поиграем'},
 ])
 
-const start = () => {
-    bot.setMyCommands([
+const start = async () => {
+    await bot.setMyCommands([
         {command: '/start', description: 'начало работы'},
         {command: '/info', description: 'информация о юзере'},
     ])
@@ -57,10 +54,10 @@ const start = () => {
             return startGame(chatId);
         }
 
-        if (data === chats[chatId]) {
-            bot.sendMessage(chatId, `Ты угадал! Цифра ${chats[chatId]}`, againOptions)
+        if (Number(data) === Number(chats[chatId])) {
+            await bot.sendMessage(chatId, `Ты угадал! Цифра ${chats[chatId]}`, againOptions)
         } else {
-            bot.sendMessage(chatId, `Ты не угадал! Цифра ${chats[chatId]}`, againOptions)
+            await bot.sendMessage(chatId, `Ты не угадал! Цифра ${chats[chatId]}`, againOptions)
         }
     })
 }
